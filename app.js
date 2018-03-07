a54195f891bc08ceeafd1660c198d41c9a48453e
Brandon Johnson <bmj1985@gmail.com>
2018-03-07 10:05:37 -0700

    required cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 const poems = require('./routes/poems');
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/poems', poems);
